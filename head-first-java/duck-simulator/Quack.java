e91fcc6d87475249800bb11589beacb36ac50e41
mahedee <mahedee.hasan@gmail.com>
2020-11-09 14:48:31 +0600

    add mini duck simulator

--- head-first-java/duck-simulator/Quack.java
+++ head-first-java/duck-simulator/Quack.java
@@ -2,6 +2,7 @@ public class Quack implements QuackBehavior{
 
     //@Override
     public void quack() {
+        
         // TODO Auto-generated method stub
         System.out.println("Quack");
 
