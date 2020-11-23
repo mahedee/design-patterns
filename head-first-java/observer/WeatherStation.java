7004af71a02e8931a76ed30512c445df2c241987
mahedee <mahedee.hasan@gmail.com>
2020-11-23 15:11:25 +0600

    observer phase-2

--- head-first-java/observer/WeatherStation.java
+++ head-first-java/observer/WeatherStation.java
@@ -2,12 +2,13 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
-        weatherData.setMeasurements(80f, 65f, 30.4f);
-
-        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
-        weatherData.setMeasurements(81f, 66f, 98.7f);
-        
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+     
+        weatherData.setMeasurements(80f, 65f, 30.4f);       
+        weatherData.setMeasurements(81f, 66f, 98.7f);
         weatherData.setMeasurements(82f, 67f, 99.4f); 
                 
     }
